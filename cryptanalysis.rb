935110f61a31fefc75cc73e2699f9b9c2038bf2a
John Russell <jjrussell42@students.tntech.edu>
2016-02-26 21:54:13 -0600

    Starting over repo

---
+++ cryptanalysis.rb
@@ -0,0 +1,68 @@
+class Cryptanalysis
+  @@alphabet = ('A'..'Z').to_a.join
+
+  @@english_frequency = { A: 0.080, B: 0.015, C: 0.030, D: 0.040, E: 0.130, F: 0.020,
+                          G: 0.015, H: 0.060, I: 0.065, J: 0.005, K: 0.005, L: 0.035,
+                          M: 0.030, N: 0.070, O: 0.080, P: 0.020, Q: 0.002, R: 0.065,
+                          S: 0.060, T: 0.090, U: 0.030, V: 0.010, W: 0.015, X: 0.005,
+                          Y: 0.020, Z: 0.002 }
+
+  def Cryptanalysis.caesar_cipher(text, shift, mode = :encrypt)
+    text      = text.upcase
+    shift     = shift % @@alphabet.size
+    decrypt   = @@alphabet
+    encrypt   = @@alphabet[shift..-1] + @@alphabet[0...shift]
+
+    text.tr(mode == :decrypt ? encrypt : decrypt,
+            mode == :decrypt ? decrypt : encrypt) #ciphertext/plaintext
+  end
+
+  def Cryptanalysis.analyze_caesar(ciphertext)
+    ciphertext_frequency      = character_frequency(ciphertext, :frequency)
+    correlation_of_frequency  = {}
+
+    (0..25).each { |i| #Potential shift amount
+      frequency = 0.0
+      @@alphabet.each_char { |c| frequency += ciphertext_frequency[c.to_sym] * @@english_frequency[@@alphabet[(26 + @@alphabet.index(c) - i) % 26].to_sym] } #Correlation of frequency formula
+
+      correlation_of_frequency[i] = frequency
+    }
+
+    correlation_of_frequency.sort_by { |_, v| -v }.take(5).to_h #Top 5 results
+  end
+
+  def Cryptanalysis.compute_ic(ciphertext)
+    chars = text_to_char_array(ciphertext)
+    occurrence = character_frequency(ciphertext, :occurrence)
+
+    sum = 0.0
+    @@alphabet.each_char { |c|
+      f_i = occurrence[c.to_sym]
+      sum += f_i * (f_i - 1.0)
+    }
+    n = chars.size
+
+    sum /= n * (n - 1.0)
+
+    sum
+  end
+
+  def Cryptanalysis.text_to_char_array(text)
+    result = text.upcase
+    result = result.gsub(/[^A-Z]/i, '').split(//)
+
+    result #Array of only uppercase alphabetical chars of the text
+  end
+  private_class_method :text_to_char_array
+
+  def Cryptanalysis.character_frequency(ciphertext, mode = :occurrence)
+    chars = text_to_char_array(ciphertext)
+    frequency = {}
+    increment = (mode == :frequency) ? 1.0 / chars.size : 1.0
+
+    @@alphabet.each_char  { |c| frequency[c.to_sym] = 0.0 }
+    chars.each            { |c| frequency[c.to_sym] += increment }
+
+    frequency #Frequency of characters in array
+  end
+end
---
+++ main.rb
@@ -0,0 +1,98 @@
+require_relative 'cryptanalysis'
+
+def caesar_encrypt
+  text = get_input('Enter the name of the file containing the text to encrypt: ', 'Enter the text to encrypt: ')
+  shift = get_cmd_entry('Enter the shift key (0-25): ').to_i
+
+  puts "Plaintext: %s\nCiphertext: %s\n" % [text, Cryptanalysis.caesar_cipher(text, shift, :encrypt)]
+end
+
+def caesar_decrypt
+  text = get_input('Enter the name of the file containing the text to decrypt: ', 'Enter the text to decrypt: ')
+  shift = get_cmd_entry('Enter the shift key (0-25): ').to_i
+
+  puts "Ciphertext: %s\nPlaintext: %s\n" % [text, Cryptanalysis.caesar_cipher(text, shift, :decrypt)]
+end
+
+def caesar_analyze
+  text = get_input('Enter the name of the file containing the text to be analyzed: ', 'Enter the text to be analyzed: ')
+  result = Cryptanalysis.analyze_caesar(text)
+
+  puts 'Top 5 possible shifts: '
+  choice = 0
+  result.each { |k, v| puts '%10s) Shift amount: %2s Correlation: %%%.4f Result: %s' % [choice += 1, k, v, Cryptanalysis.caesar_cipher(text, k, :decrypt)] }
+end
+
+def compute_ic
+  text = get_input('Enter the name of the file containing the text to be analyzed: ', 'Enter the text to be analyzed: ')
+  puts "Text: %s\nIC: %.4f" % [text, Cryptanalysis.compute_ic(text)]
+end
+
+def compute_occurrence
+  text = get_input('Enter the name of the file containing the text to be analyzed: ', 'Enter the text to be analyzed: ')
+  puts "Text: %s\nOccurrences: %s" % [text, Cryptanalysis.character_frequency(text, :occurrence)]
+end
+
+def compute_frequency
+  text = get_input('Enter the name of the file containing the text to be analyzed: ', 'Enter the text to be analyzed: ')
+  puts "Text: %s\nFrequencies: %s" % [text, Cryptanalysis.character_frequency(text, :frequency)]
+end
+
+def get_input(file_message, cmd_message)
+  (get_input_mode == :file) ? get_file_contents(file_message) : get_cmd_entry(cmd_message)
+end
+
+def get_input_mode
+  puts "Enter how the data will be input:\n1) Command line\n2) Text File"
+  user_choice = gets.chomp
+
+  user_choice == '1' ? :cmd : :file
+end
+
+def get_file_contents(message)
+  puts message
+  filename = gets.chomp
+  File.open(filename).read
+end
+
+def get_cmd_entry(message)
+  puts message
+  gets.chomp
+end
+
+menu = '
+1) Encrypt a string with a Caesar cipher
+2) Decrypt a Caesar cipher with the key
+3) Analyze a Caesar cipher to find the top 5 possible keys
+4) Compute the Index of Coincidence for a string
+5) Compute the number of times each letter occurs in a string
+6) Compute the frequency at which each letter occurs in a string
+0) Quit'
+
+loop = true
+while loop
+  puts "#####Cryptanalysis Driver#####\n%s" % [menu]
+  userchoice = gets.chomp
+
+  case userchoice
+    when '1'
+      caesar_encrypt
+    when '2'
+      caesar_decrypt
+    when '3'
+      caesar_analyze
+    when '4'
+      compute_ic
+    when '5'
+      compute_occurrence
+    when '6'
+      compute_frequency
+    when '0'
+      puts 'exiting..'
+      loop = false
+    else
+
+  end
+end
+
+
