3ac5c17c7b83573a2cbd7ecdddfd6736e598412e
John Russell <jjrussell42@students.tntech.edu>
2016-02-27 02:23:22 -0600

    Refactoring

--- cryptanalysis.rb
+++ cryptanalysis.rb
@@ -1,17 +1,17 @@
 class Cryptanalysis
-  @@alphabet = ('A'..'Z').to_a.join
+  ALPHABET = ('A'..'Z').to_a.join
 
-  @@english_frequency = { A: 0.080, B: 0.015, C: 0.030, D: 0.040, E: 0.130, F: 0.020,
-                          G: 0.015, H: 0.060, I: 0.065, J: 0.005, K: 0.005, L: 0.035,
-                          M: 0.030, N: 0.070, O: 0.080, P: 0.020, Q: 0.002, R: 0.065,
-                          S: 0.060, T: 0.090, U: 0.030, V: 0.010, W: 0.015, X: 0.005,
-                          Y: 0.020, Z: 0.002 }
+  ENGLISH_FREQUENCY = { A: 0.080, B: 0.015, C: 0.030, D: 0.040, E: 0.130, F: 0.020,
+                        G: 0.015, H: 0.060, I: 0.065, J: 0.005, K: 0.005, L: 0.035,
+                        M: 0.030, N: 0.070, O: 0.080, P: 0.020, Q: 0.002, R: 0.065,
+                        S: 0.060, T: 0.090, U: 0.030, V: 0.010, W: 0.015, X: 0.005,
+                        Y: 0.020, Z: 0.002 }
 
   def Cryptanalysis.caesar_cipher(text, shift, mode = :encrypt)
     text      = text.upcase
-    shift     = shift % @@alphabet.size
-    decrypt   = @@alphabet
-    encrypt   = @@alphabet[shift..-1] + @@alphabet[0...shift]
+    shift     = shift % ALPHABET.size
+    decrypt   = ALPHABET
+    encrypt   = ALPHABET[shift..-1] + ALPHABET[0...shift]
 
     text.tr(mode == :decrypt ? encrypt : decrypt,
             mode == :decrypt ? decrypt : encrypt) #ciphertext/plaintext
@@ -33,7 +33,9 @@ class Cryptanalysis
 
     (0..25).each { |i| #Potential shift amount
       frequency = 0.0
-      @@alphabet.each_char { |c| frequency += ciphertext_frequency[c.to_sym] * @@english_frequency[@@alphabet[(26 + @@alphabet.index(c) - i) % 26].to_sym] } #Correlation of frequency formula
+      ALPHABET.each_char { |c| frequency += ciphertext_frequency[c.to_sym] *
+                                        ENGLISH_FREQUENCY[ALPHABET[(ALPHABET.size + ALPHABET.index(c) - i) %
+                                                     ALPHABET.size].to_sym] }
 
       correlation_of_frequency[i] = frequency
     }
@@ -46,7 +48,7 @@ class Cryptanalysis
     occurrence  = character_frequency(ciphertext, :occurrence)
 
     sum = 0.0
-    @@alphabet.each_char { |c|
+    ALPHABET.each_char { |c|
       f_i = occurrence[c.to_sym]
       sum += f_i * (f_i - 1.0)
     }
@@ -59,20 +61,16 @@ class Cryptanalysis
 
   def Cryptanalysis.text_to_char_array(text)
     result = text.upcase
-    result = result.gsub(/[^A-Z]/i, '').split(//)
-
-    result #Array of only uppercase alphabetical chars of the text
+    result.gsub(/[^A-Z]/i, '').split(//)
   end
   private_class_method :text_to_char_array
 
   def Cryptanalysis.character_frequency(ciphertext, mode = :occurrence)
-    chars = text_to_char_array(ciphertext)
-    frequency = {}
-    increment = (mode == :frequency) ? 1.0 / chars.size : 1.0
-
-    @@alphabet.each_char  { |c| frequency[c.to_sym] = 0.0 }
-    chars.each            { |c| frequency[c.to_sym] += increment }
-
-    frequency
+    chars     = text_to_char_array(ciphertext)
+    default   = ENGLISH_FREQUENCY.map{|k,_| [k, 0]}.to_h
+    freqs     = chars.group_by {|c| c}
+                    .map {|k,v| [k.to_sym, v.size.to_f / ((mode == :occurrence) ? 1.0 : chars.size)]}
+                    .to_h
+    default.merge(freqs)
   end
 end
